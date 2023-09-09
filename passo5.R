1c354a35d4f74d69323795143ca3ab1b967b8b56
ludihan <luccad.han@gmail.com>
2023-09-09 17:59:47 -0300

    passo5

---
+++ passo5.R
@@ -0,0 +1,6 @@
+data.frame(enem)
+attach(enem)
+
+notaCienciasDaNatureza=table(enem$`Nota da prova de Ciências da Natureza`)
+notaCienciasHumanas=table(enem$`Nota da prova de Ciências da Natureza`)
+notaLinguagensECodigos=table(enem$`Nota da prova de Ciências da Natureza`)
