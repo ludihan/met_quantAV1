2354e3874dc48fd7eb8b6ef0f4e42b828c823aa9
matheus cruz <matheusnajalk@gmail.com>
2023-09-09 18:08:41 -0300

    já toma o passo 4

---
+++ passo4.R
@@ -0,0 +1,23 @@
+library(ggplot2)
+
+dados = data.frame(
+  Categoria = c(enem$`Na sua residência tem acesso à Internet?`),
+  Contagem = c(enem$`Na sua residência tem acesso à Internet?`)
+)
+
+ggplot(dados, aes(x = "", y = Contagem, fill = Categoria)) +
+  geom_bar(stat = "identity") +
+  coord_polar(theta = "y") +
+  labs(x = NULL, y = NULL) +
+  theme_void()
+
+dados2 = data.frame(
+  Categoria = c(enem$`Na sua residência tem telefone celular?`),
+  Contagem = c(enem$`Na sua residência tem telefone celular?`)
+)
+
+ggplot(dados2, aes(x = "", y = Contagem, fill = Categoria)) +
+  geom_bar(stat = "identity") +
+  coord_polar(theta = "y") +
+  labs(x = NULL, y = NULL) +
+  theme_void()
