27330d2e97e0c4524702ef56a35b201f8dd13b91
ludihan <luccad.han@gmail.com>
2023-09-15 17:59:03 -0300

    fix passo1

--- passo1.R
+++ passo1.R
@@ -8,25 +8,31 @@ estadoCivil
 
 dependenciaAdministrativaSimples=table(enem$`Dependência administrativa (Escola)`)
 dependenciaAdministrativaRelativa = prop.table(dependenciaAdministrativaSimples)
-dependenciaAdministrativa = cbind(simples = dependenciaAdministrativaSimples,relativa = dependenciaAdministrativaRelativa)
+dependenciaAdministrativa = cbind(simples = dependenciaAdministrativaSimples,
+                                  relativa = dependenciaAdministrativaRelativa)
 dependenciaAdministrativa
 
 rendaMensalSimples=table(enem$`Qual é a renda mensal de sua família?`)
 rendaMensalRelativa = prop.table(rendaMensalSimples)
-rendaMensal = cbind(simples = rendaMensalSimples,relativa = rendaMensalRelativa)
+rendaMensal = cbind(simples = rendaMensalSimples,
+                    relativa = rendaMensalRelativa)
 rendaMensal
 
 acessoInternetSimples=table(enem$`Na sua residência tem acesso à Internet?`)
 acessoInternetRelativa = prop.table(acessoInternetSimples)
-acessoInternet = cbind(simples = acessoInternetSimples,relativa = acessoInternetRelativa)
+acessoInternet = cbind(simples = acessoInternetSimples,
+                       relativa = acessoInternetRelativa)
 acessoInternet
 
 linguaEstrangeiraSimples=table(enem$`Língua Estrangeira`)
 linguaEstrangeiraRelativa= prop.table(linguaEstrangeiraSimples)
-linguaEstrangeira = cbind(simples = linguaEstrangeiraSimples,relativa = linguaEstrangeiraSimples)
+linguaEstrangeira = cbind(simples = linguaEstrangeiraSimples,
+                          relativa = linguaEstrangeiraRelativa)
 linguaEstrangeira
 
 ensinoMedioSimples=table(enem$`Tipo de escola do Ensino Médio`)
 ensinoMedioRelativa= prop.table(ensinoMedioSimples)
-ensinoMedio = cbind(simples = ensinoMedioSimples,relativa = ensinoMedioRelativa)
+ensinoMedio = cbind(simples = ensinoMedioSimples,
+                    relativa = ensinoMedioRelativa)
 ensinoMedio
+
